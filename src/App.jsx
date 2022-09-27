334e53c04ab796134b82c68fdc807a0b323d4689
kihyeoon <bangion93@gmail.com>
2022-09-27 14:19:56 +0900

    🎉 initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Style from "./App.css";
+
+const App = () => {
+  return <h1 className="title">Hello World!</h1>;
+};
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    "js/app": ["./src/App.jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "docs/"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "common.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
